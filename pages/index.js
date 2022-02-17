21b6ebd893e2517e26b2c3b218136faa97dfe0a4
Jose Angel <kikomalbranche@gmail.com>
2022-02-17 20:43:19 -0300

    adicionando a jose como nuevo desarrollador

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ export default function Home() {
     <div>
       <h3>Proyecto desarrollado por:</h3>
       <ul>
-        <li>Hermano Leite: <i>"Creyo que sera una buena oportunidad para practicarmos lo desarrollo de un sistema con cara de real"</i></li>
+        <li>Hermano Leite: <i>"Creo que será una buena oportunidad para practicar el desarrollo como un sistema real"</i></li>
+        <li>Jose Angel: <i>"Todas las personas deberían aprender a programar, porque les enseña a pensar"</i> </li>
       </ul>
     </div>
   )
